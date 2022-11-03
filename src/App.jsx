800bdd7678d64a2258ec91e8b4f7821914d99a8e
Raheem Kawojue <kawojue08@gmail.com>
2022-11-04 00:27:10 +0100

    Edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Faqs from "./components/Faqs"
+
 function App() {
   return (
     <main className="card">
+      <h3 className="md:text-2xl">
+        questions and answers about login
+      </h3>
 
+      <section>
+        <Faqs />
+      </section>
     </main>
   )
 }
