b07a62853a6b2c8ad3f4854ae158379e82af3078
Raheem Kawojue <kawojue08@gmail.com>
2022-11-04 01:11:26 +0100

    setup components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import Faqs from "./components/Faqs"
+import faqs from './faqs'
+import { useState } from 'react'
+import Faq from "./components/Faq"
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <main className="card">
       <h3 className="md:text-2xl">
         questions and answers about login
       </h3>
-
-      <section>
-        <Faqs />
-      </section>
+      <Faq faqs={faqs} open={isOpen} setOpen={setIsOpen} />
     </main>
   )
 }
