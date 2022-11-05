1a3ed7927ef6790618152b6ca842d6b6c2dc48e7
Raheem Kawojue <kawojue08@gmail.com>
2022-11-05 09:29:25 +0100

    Edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import faqs from './faqs'
+import data from './data'
 import { useState } from 'react'
 import Faq from "./components/Faq"
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
+
+  const [faqs, setFaqs] = useState(data)
 
   return (
     <main className="card">
       <h3 className="md:text-2xl">
         questions and answers about login
       </h3>
-      <Faq faqs={faqs} open={isOpen} setOpen={setIsOpen} />
+      <section className="cards-container">
+        {faqs.map(faq => (
+          <Faq {...faq} key={faq.id} />
+        ))}
+      </section>
     </main>
   )
 }
