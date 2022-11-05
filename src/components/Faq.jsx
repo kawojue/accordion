18b17ed2e4d68bce7ee7950b86ba3cf8bd99a8b9
Raheem Kawojue <kawojue08@gmail.com>
2022-11-05 09:29:03 +0100

    moved state

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,28 +1,27 @@
+import { useState } from 'react'
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
 
 
-const Faq = ({ faqs, open, setOpen }) => {
+const Faq = ({ quest, ans }) => {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
-        <section className="cards-container">
-            {faqs.map(faq => (
-                <article key={faq.id} className="cards">
-                    <div className="cards-quest">
-                        <h5 className="md:text-[1.2rem] md:leading-normal">
-                            {faq.quest}
-                        </h5>
-                        <div className="icon-container" onClick={() => setOpen(!open)}>
-                            {open ?
-                                <AiOutlineMinus /> :
-                                <AiOutlinePlus />}
-                        </div>
-                    </div>
-                    {open &&
-                        <p className="md:text-[0.9rem]">
-                            {faq.ans}
-                        </p>}
-                </article>
-            ))}
-        </section>
+        <article className="cards">
+            <div className="cards-quest">
+                <h5 className="md:text-[1.2rem] md:leading-normal">
+                    {quest}
+                </h5>
+                <div className="icon-container" onClick={() => setIsOpen(!isOpen)}>
+                    {isOpen ?
+                        <AiOutlineMinus /> :
+                        <AiOutlinePlus />}
+                </div>
+            </div>
+            {isOpen &&
+                <p className="md:text-[0.9rem]">
+                    {ans}
+                </p>}
+        </article>
     )
 }
 
