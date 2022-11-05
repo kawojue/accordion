8a0d766d6f6e706496fc076f2ffc499e9d892cac
Raheem Kawojue <kawojue08@gmail.com>
2022-11-05 09:29:43 +0100

    Renamed..

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        id: 1,
+        quest: "Do I have to allow the use of cookes?",
+        ans: "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art."
+    },
+    {
+        id: 2,
+        quest: "How do I change my Page password?",
+        ans: "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse."
+    },
+    {
+        id: 3,
+        quest: "What is BankID?",
+        ans: "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial."
+    },
+    {
+        id: 4,
+        quest: "Whose birth number can I use?",
+        ans: "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify."
+    },
+    {
+        id: 5,
+        quest: "When do I recieve a password ordered by letter?",
+        ans: "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan"
+    }
+]
+
+export default data
